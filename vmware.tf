ee24ab23ada793795cf340ac5f6944437905cf86
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-27 23:11:55 +0530

    Update vmware.tf

--- vmware.tf
+++ vmware.tf
@@ -1,11 +1,3 @@
-provider "vsphere" {
-  user           = "${var.user}"
-  password       = "${var.password}"
-  vsphere_server = "${var.host}"
-
-  allow_unverified_ssl = true
-}
-
 data "vsphere_datacenter" "dc" {
   name = "${var.region}"
 }
