03d02023044393173ee17bc05626546138c7ad21
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-28 21:53:49 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -2,6 +2,6 @@ provider "vsphere" {
   user           = "${var.user}"
   password       = "${var.password}"
   vsphere_server = "${var.host}"
-
+  version = "< 1.16.0"
   allow_unverified_ssl = true
 }
